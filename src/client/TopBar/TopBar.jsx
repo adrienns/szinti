3acea005494859568b86fc99830d4606f3807941
Adrienn Sepa <adrienn.sepa@outlook.com>
2021-01-19 23:44:08 +0200

    topbar changing text

--- src/client/TopBar/TopBar.jsx
+++ src/client/TopBar/TopBar.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./TopBar.css";
 
+const TEXTS = [
+  "Személyes átvételi lehetőség Budapesten",
+  "Ingyenes szallitas barhol 19 000ft felett",
+];
+
 const TopBar = () => {
+  const [currentText, setCurrentText] = useState(0);
+
+  const changeText = () => {
+    const textlength = TEXTS.length;
+    const nextState = (currentText + 1) % textlength;
+    setCurrentText(nextState);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      changeText();
+    }, 3000);
+    return () => clearInterval(interval);
+  }, [currentText]);
+
+  const text = TEXTS[currentText];
   return (
     <div>
-      <span className="topbar-container">Ingyenes szállítás Budapesten!</span>
+      <span className="topbar-container">{text}</span>
     </div>
   );
 };
