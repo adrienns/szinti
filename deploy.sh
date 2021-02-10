400f316f5e309270b3ec2dedc2dea47e71269dfe
Adrienn Sepa <adrienn.sepa@outlook.com>
2021-02-11 01:15:27 +0200

    small change

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 ssh -i "~/.ssh/szintis.pem" ubuntu@ec2-18-188-66-127.us-east-2.compute.amazonaws.com << EOF
    cd /home/ubuntu/szinti
    git pull
-   sudo service nginx reload
+    if [ -f /run/nginx.pid ]; then
+        sudo service nginx reload
+    else
+        /usr/bin/nginx
+    fi
+   
    pm2 start ps2-config.json
 EOF
