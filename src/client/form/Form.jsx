e02cb36091392961aac6df87b196002be19adae7
Adrienn Sepa <adrienn.sepa@outlook.com>
2021-02-17 10:45:59 +0200

    fixing api

--- src/client/form/Form.jsx
+++ src/client/form/Form.jsx
@@ -52,7 +52,7 @@ class Form extends Component {
       message: this.state.message,
     };
     axios
-      .post(`$/api/form`, data)
+      .post(`/api/form`, data)
       .then((res) => {
         this.setState(
           {
